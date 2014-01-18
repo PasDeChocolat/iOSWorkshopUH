3715a065552cd6a6c4c9d2120d765ab83cb46f6d
Kyle Oba <koba@mudphone.com>
2014-01-18 08:43:32 -1000

    Move to separate directories.

---
+++ DEMOS/demo/demo/AAViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAViewController.h
+//  demo
+//
+//  Created by Kyle Oba on 1/18/14.
+//  Copyright (c) 2014 AgencyAgency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAViewController : UIViewController
+
+@end
---
+++ DEMOS/demo/demo/AAViewController.m
@@ -0,0 +1,29 @@
+//
+//  AAViewController.m
+//  demo
+//
+//  Created by Kyle Oba on 1/18/14.
+//  Copyright (c) 2014 AgencyAgency. All rights reserved.
+//
+
+#import "AAViewController.h"
+
+@interface AAViewController ()
+
+@end
+
+@implementation AAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
